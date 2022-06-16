d91d8bf5b0b1085f325be647f66a646b9b61a593
wlcross <william589@revature.net>
2022-06-16 15:38:23 -0400

    Finished AccountDAO

--- src/main/java/dev/cross/repositories/AccountDAO.java
+++ src/main/java/dev/cross/repositories/AccountDAO.java
@@ -81,4 +81,56 @@ private static ConnectionUtil cu = ConnectionUtil.getConnectionUtil();
 		return accounts;
 	}
 	
+	public Account getAccountById(int id) {
+		String sql = "select * from bankingapp.accounts where id = ?";
+		Account a = null;
+		try (Connection conn = cu.getConnection()) {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1,  id);;
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				a = new Account(rs.getInt("id"), rs.getBoolean("savings"), rs.getInt("balance"), rs.getInt("user_id"));
+			} else {
+				a = null;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return a;
+	}
+	
+	public boolean updateAccount(Account updatedAccount) {
+		String sql = "update bankingapp.accounts set (savings, balance, user_id)" + "= (?, ?, ?) where id = ?";
+		try (Connection conn = cu.getConnection()) {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setBoolean(1, updatedAccount.getSavings());
+			ps.setInt(2, updatedAccount.getBalance());
+			ps.setInt(3, updatedAccount.getUserID());
+			ps.setInt(4, updatedAccount.getId());
+			if (ps.executeUpdate() == 0) {
+				return false;
+			} else {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean deleteAccount (int id) {
+		String sql = "delete from bankingapp.accounts where id = ?";
+		try (Connection conn = cu.getConnection()) {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			if (ps.executeUpdate() == 0) {
+				return false;
+			} else {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 }
